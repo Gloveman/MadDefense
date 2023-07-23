c468114806b8a888def578157885ebb9cc63a14d
gloveman <gloveman@postech.ac.kr>
2023-07-23 18:41:07 +0900

    server changed

--- server.js
+++ server.js
@@ -1,10 +1,57 @@
+
 var wsServer=require("ws").Server
 var wss=new wsServer({port:5001},()=>{console.log("listening 5001....")})
 
+let map={
+    data:[
+        {
+            points:[-5,-3],
+            tile:1
+        },
+        {
+            points:[-4,-3],
+            tile:1
+        },
+        {
+            points:[-3,-3],
+            tile:1
+        },
+        {
+            points:[-2,-3],
+            tile:1
+        },
+        {
+            points:[-1,-3],
+            tile:1
+        },
+        {
+            points:[0,-3],
+            tile:1
+        },
+        {
+            points:[5,-3],
+            tile:1
+        },
+        {
+            points:[4,-3],
+            tile:1
+        },
+        {
+            points:[3,-3],
+            tile:1
+        },
+        {
+            points:[2,-3],
+            tile:1
+        },
+        {
+            points:[1,-3],
+            tile:1
+        }
+    ]
+}
 
 wss.on('connection',(client)=>{
-    client.on('message',(message)=>{
-        console.log("client: %s",message)
-    });
-    client.send("From node.js");
+
+    client.send(JSON.stringify(map));
 });
