c2081b388c9f91d0eccb29345efefb056698792f
2022070319 <wisecow703@hanyang.ac.kr>
2023-07-25 19:14:13 +0900

    제발

--- server.js
+++ server.js
@@ -2,54 +2,6 @@ const fs=require('fs');
 var wsServer=require("ws").Server
 var wss=new wsServer({port:5001},()=>{console.log("listening 5001....")})
 
-// let map={
-//     data:[
-//         {
-//             points:[-5,0],
-//             tile:1
-//         },
-//         {
-//             points:[-4,0],
-//             tile:1
-//         },
-//         {
-//             points:[-3,0],
-//             tile:1
-//         },
-//         {
-//             points:[-2,0],
-//             tile:1
-//         },
-//         {
-//             points:[-1,0],
-//             tile:1
-//         },
-//         {
-//             points:[0,0],
-//             tile:1
-//         },
-//         {
-//             points:[5,-0],
-//             tile:1
-//         },
-//         {
-//             points:[4,0],
-//             tile:1
-//         },
-//         {
-//             points:[3,0],
-//             tile:1
-//         },
-//         {
-//             points:[2,0],
-//             tile:1
-//         },
-//         {
-//             points:[1,0],
-//             tile:1
-//         }
-//     ]
-// }
 
 
 wss.on('connection',(client)=>{
